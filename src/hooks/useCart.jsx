6e280f4b0c97061fe00221661f0f2b0c42105a40
habibullahraju <raju.ra423@gmail.com>
2023-06-09 02:14:38 +0600

    i added cart custom hook and fetch data show navbar

---
+++ src/hooks/useCart.jsx
@@ -0,0 +1,21 @@
+import {useQuery} from "@tanstack/react-query";
+import {useContext} from "react";
+import {AuthContext} from "../Providers/AuthProvider";
+import useAxiosSecure from "./useAxiosSecure";
+const useCart = () => {
+  const {user, loading} = useContext(AuthContext);
+  const [axiosSecure] = useAxiosSecure();
+
+  const {refetch, data: cart = []} = useQuery({
+    queryKey: ["carts", user?.email],
+    enabled: !loading,
+    queryFn: async () => {
+      const res = await axiosSecure(`/carts?email=${user?.email}`);
+      return res.data;
+    },
+  });
+
+  return [cart, refetch];
+};
+
+export default useCart;
--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import React, {useContext} from "react";
 import {Link} from "react-router-dom";
 import {AuthContext} from "../../../Providers/AuthProvider";
+import useCart from "../../../hooks/useCart";
 
 const Navbar = () => {
   const {user, logOut} = useContext(AuthContext);
+  const [cart] = useCart();
+  console.log(cart);
   const navOptions = (
     <>
       <li>
@@ -78,7 +81,7 @@ const Navbar = () => {
                     d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                   />
                 </svg>
-                <span className="badge badge-sm indicator-item">8</span>
+                <span className="badge badge-sm indicator-item">{cart.length}</span>
               </div>
             </label>
           </div>
